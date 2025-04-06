5fbf94be46a4702a87b71054f32670310eef9c55
yamada-y0 <yama102130@gmail.com>
2025-04-06 22:54:44 +0900

    Refactor domain model package structure and rename voting methods for clarity

--- quarkus-app/src/main/java/com/github/yamay0/application/port/in/VoteFruitUseCase.java
+++ quarkus-app/src/main/java/com/github/yamay0/application/port/in/VoteFruitUseCase.java
@@ -1,10 +1,10 @@
 package com.github.yamay0.application.port.in;
 
-import com.github.yamay0.domain.model.Fruit;
-import com.github.yamay0.domain.model.UserId;
+import com.github.yamay0.application.domain.model.Fruit;
+import com.github.yamay0.application.domain.model.UserId;
 
 import java.util.List;
 
 public interface VoteFruitUseCase {
-    void vote(List<Fruit> fruits, UserId userId);
+    void execute(List<Fruit> fruits, UserId userId);
 }
