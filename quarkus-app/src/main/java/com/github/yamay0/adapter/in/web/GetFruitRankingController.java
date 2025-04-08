7307131b8879677765f342bda1408ae78319ac7d
yamada-y0 <yama102130@gmail.com>
2025-04-09 01:04:09 +0900

    Add JSON response support to GetFruitRankingController

--- quarkus-app/src/main/java/com/github/yamay0/adapter/in/web/GetFruitRankingController.java
+++ quarkus-app/src/main/java/com/github/yamay0/adapter/in/web/GetFruitRankingController.java
@@ -4,10 +4,13 @@ import com.github.yamay0.application.domain.model.FruitRankEntry;
 import com.github.yamay0.application.port.in.GetFruitRankUseCase;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
 
 @Path("/fruit-ranking")
+@Produces(MediaType.APPLICATION_JSON)
 public class GetFruitRankingController {
     private final GetFruitRankUseCase getFruitRankUseCase;
 
