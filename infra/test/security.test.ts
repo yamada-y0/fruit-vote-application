15a87f7057d5c8a72613d9180ae69f9923605f9f
yamada-y0 <yama102130@gmail.com>
2025-04-13 22:27:14 +0900

    Add Prettier configuration and format code; update package dependencies

--- infra/test/security.test.ts
+++ infra/test/security.test.ts
@@ -1,17 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import {Template} from 'aws-cdk-lib/assertions';
-import * as Infra from '../lib/security-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as Infra from "../lib/security-stack";
 
-test('IAM Role Created', () => {
+test("IAM Role Created", () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Infra.SecurityStack(app, 'SecurityTestStack');
+    const stack = new Infra.SecurityStack(app, "SecurityTestStack");
 
     // THEN
     const template = Template.fromStack(stack);
 
-    template.hasResourceProperties('AWS::IAM::Role', {
-        RoleName: 'GitHubActionsRole',
+    template.hasResourceProperties("AWS::IAM::Role", {
+        RoleName: "GitHubActionsRole",
         MaxSessionDuration: 7200,
     });
 });
