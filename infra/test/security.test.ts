75aea6ef593fad71dbe50be9ce8124f88d8dd1ed
yamada-y0 <yama102130@gmail.com>
2025-04-13 13:39:00 +0900

    Add security stack and IAM role for GitHub Actions integration

---
+++ infra/test/security.test.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import {Template} from 'aws-cdk-lib/assertions';
+import * as Infra from '../lib/security-stack';
+
+test('IAM Role Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Infra.SecurityStack(app, 'SecurityTestStack');
+
+    // THEN
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::IAM::Role', {
+        RoleName: 'GitHubActionsRole',
+        MaxSessionDuration: 7200,
+    });
+});
