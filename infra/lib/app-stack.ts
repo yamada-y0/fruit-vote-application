ff089fa72b3aeafc1594a1ab569babeac5287f67
yamada-y0 <yama102130@gmail.com>
2025-04-13 19:31:32 +0900

    Add VPC and ECS cluster setup with Fargate service; update application properties

--- infra/lib/app-stack.ts
+++ infra/lib/app-stack.ts
@@ -2,18 +2,39 @@ import * as cdk from 'aws-cdk-lib';
 import {StackProps} from 'aws-cdk-lib';
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import {Construct} from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 
 export interface AppStackProps extends StackProps {
     appName: string;
+    vpc: ec2.IVpc;
 }
 
 export class AppStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: AppStackProps) {
         super(scope, id, props);
-        new ecr.Repository(this, 'EcrRepository', {
+        const repository = new ecr.Repository(this, 'EcrRepository', {
             repositoryName: `${props.appName}-app`,
             removalPolicy: cdk.RemovalPolicy.DESTROY,
             emptyOnDelete: true,
         });
+
+        const cluster = new ecs.Cluster(this, 'EcsCluster', {
+            vpc: props.vpc,
+            clusterName: `${props.appName}-cluster`,
+        });
+        new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'FargateService', {
+            cluster: cluster,
+            cpu: 256,
+            desiredCount: 1,
+            taskImageOptions: {
+                image: ecs.ContainerImage.fromEcrRepository(repository),
+                containerPort: 8080,
+                enableLogging: true,
+            },
+            memoryLimitMiB: 512,
+            publicLoadBalancer: false,
+        })
     }
 }
---
+++ infra/lib/network-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export class NetworkStack extends cdk.Stack {
+    public readonly vpc: ec2.IVpc;
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        this.vpc = new ec2.Vpc(this, 'VPC', {
+            ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: 'Isolated',
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                },
+            ],
+            maxAzs: 2,
+        });
+
+        new ec2.InterfaceVpcEndpoint(this, 'EcrApiEndpoint', {
+            vpc: this.vpc,
+            service: ec2.InterfaceVpcEndpointAwsService.ECR,
+        });
+        new ec2.InterfaceVpcEndpoint(this, 'EcrDockerEndpoint', {
+            vpc: this.vpc,
+            service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+        });
+        new ec2.InterfaceVpcEndpoint(this, 'CloudWatchLogsEndpoint', {
+            vpc: this.vpc,
+            service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+        });
+        new ec2.GatewayVpcEndpoint(this, 'S3Endpoint', {
+            vpc: this.vpc,
+            service: ec2.GatewayVpcEndpointAwsService.S3,
+        });
+    }
+}
