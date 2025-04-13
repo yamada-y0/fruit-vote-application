615929ea3211a9fc7a76f4dd5120eae39e7f95a1
yamada-y0 <yama102130@gmail.com>
2025-04-13 22:17:04 +0900

    Update application tests to include network stack and validate ECS resources

--- infra/test/app.test.ts
+++ infra/test/app.test.ts
@@ -1,19 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import {Template} from 'aws-cdk-lib/assertions';
-import * as Infra from '../lib/app-stack';
+import * as App from '../lib/app-stack';
+import * as Network from '../lib/network-stack';
 
-test('ECR Repository Created', () => {
+test('Application Resources Created', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Infra.AppStack(app, 'AppTestStack', {
+    const networkStack = new Network.NetworkStack(app, 'NetworkTestStack');
+    const appStack = new App.AppStack(app, 'AppTestStack', {
         appName: 'quarkus',
+        vpc: networkStack.vpc,
     });
 
     // THEN
-    const template = Template.fromStack(stack);
+    const template = Template.fromStack(appStack);
 
     template.hasResourceProperties('AWS::ECR::Repository', {
         RepositoryName: 'quarkus-app',
         EmptyOnDelete: true,
+    });
+    template.hasResourceProperties('AWS::ECS::Cluster', {
+        ClusterName: 'quarkus-cluster',
+    });
+    template.hasResourceProperties('AWS::ECS::Service', {
+        DesiredCount: 1,
+    });
+    template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+        ContainerDefinitions: [
+            {
+                PortMappings: [
+                    {
+                        ContainerPort: 8080,
+                    },
+                ],
+            },
+        ],
     });
 });
