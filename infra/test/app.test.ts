ef4264deb44472163e909d7625feb7b814f60e15
yamada-y0 <yama102130@gmail.com>
2025-04-13 22:48:25 +0900

    Add unit tests for AppStack, NetworkStack, and SecurityStack with snapshot validation

--- infra/test/app.test.ts
+++ infra/test/app.test.ts
@@ -3,37 +3,53 @@ import { Template } from "aws-cdk-lib/assertions";
 import * as App from "../lib/app-stack";
 import * as Network from "../lib/network-stack";
 
-test("Application Resources Created", () => {
-    const app = new cdk.App();
-    // WHEN
-    const networkStack = new Network.NetworkStack(app, "NetworkTestStack");
-    const appStack = new App.AppStack(app, "AppTestStack", {
-        appName: "quarkus",
-        vpc: networkStack.vpc,
+describe("AppStack Test", () => {
+    test("Snapshot Test", () => {
+        const app = new cdk.App();
+        // WHEN
+        const networkStack = new Network.NetworkStack(app, "NetworkTestStack");
+        const appStack = new App.AppStack(app, "AppTestStack", {
+            appName: "quarkus",
+            vpc: networkStack.vpc,
+        });
+
+        // THEN
+        const template = Template.fromStack(appStack);
+        expect(template.toJSON()).toMatchSnapshot();
     });
 
-    // THEN
-    const template = Template.fromStack(appStack);
+    test("Application Resources Created", () => {
+        const app = new cdk.App();
+        // WHEN
+        const networkStack = new Network.NetworkStack(app, "NetworkTestStack");
+        const appStack = new App.AppStack(app, "AppTestStack", {
+            appName: "quarkus",
+            vpc: networkStack.vpc,
+        });
 
-    template.hasResourceProperties("AWS::ECR::Repository", {
-        RepositoryName: "quarkus-app",
-        EmptyOnDelete: true,
-    });
-    template.hasResourceProperties("AWS::ECS::Cluster", {
-        ClusterName: "quarkus-cluster",
-    });
-    template.hasResourceProperties("AWS::ECS::Service", {
-        DesiredCount: 1,
-    });
-    template.hasResourceProperties("AWS::ECS::TaskDefinition", {
-        ContainerDefinitions: [
-            {
-                PortMappings: [
-                    {
-                        ContainerPort: 8080,
-                    },
-                ],
-            },
-        ],
+        // THEN
+        const template = Template.fromStack(appStack);
+
+        template.hasResourceProperties("AWS::ECR::Repository", {
+            RepositoryName: "quarkus-app",
+            EmptyOnDelete: true,
+        });
+        template.hasResourceProperties("AWS::ECS::Cluster", {
+            ClusterName: "quarkus-cluster",
+        });
+        template.hasResourceProperties("AWS::ECS::Service", {
+            DesiredCount: 1,
+        });
+        template.hasResourceProperties("AWS::ECS::TaskDefinition", {
+            ContainerDefinitions: [
+                {
+                    PortMappings: [
+                        {
+                            ContainerPort: 8080,
+                        },
+                    ],
+                },
+            ],
+        });
     });
 });
--- infra/test/security.test.ts
+++ infra/test/security.test.ts
@@ -1,17 +1,29 @@
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import * as Infra from "../lib/security-stack";
+import * as Security from "../lib/security-stack";
 
-test("IAM Role Created", () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Infra.SecurityStack(app, "SecurityTestStack");
+describe("SecurityStack Test", () => {
+    test("Snapshot Test", () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new Security.SecurityStack(app, "SecurityTestStack");
 
-    // THEN
-    const template = Template.fromStack(stack);
+        // THEN
+        const template = Template.fromStack(stack);
+        expect(template.toJSON()).toMatchSnapshot();
+    });
 
-    template.hasResourceProperties("AWS::IAM::Role", {
-        RoleName: "GitHubActionsRole",
-        MaxSessionDuration: 7200,
+    test("IAM Role Created", () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new Security.SecurityStack(app, "SecurityTestStack");
+
+        // THEN
+        const template = Template.fromStack(stack);
+
+        template.hasResourceProperties("AWS::IAM::Role", {
+            RoleName: "GitHubActionsRole",
+            MaxSessionDuration: 7200,
+        });
     });
 });
